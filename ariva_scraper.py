bc92fa588d39589e74423d0ebd9dead6080f5a1c
hija <hija@users.noreply.github.com>
2019-06-12 18:35:21 +0200

    price + issn / wkn extraction

--- ariva_scraper.py
+++ ariva_scraper.py
@@ -11,6 +11,35 @@ class ArrivaScraper:
         self.request_session = requests.Session()
         self.request_session.headers = {'User-Agent': 'Ariva Scraper 0.1'}
 
+    def scrape(self, searchparameter):
+        #stock_url = self._find_stock_url(searchparameter)
+        stock_url = 'https://www.ariva.de/microsoft-aktie'
+
+        scraped_content = {'price': None}
+
+        ## Get Basic Information
+        basic_data_src = self.request_session.get(stock_url)
+        soup = BeautifulSoup(basic_data_src.text, 'html.parser')
+
+        print('{0} {1}'.format(*ArrivaScraper._extract_price(soup)))
+
+
+    @staticmethod
+    def _extract_price(soupobj):
+        price_obj = soupobj.find('span', attrs={'itemprop': 'price'})
+        price = float(price_obj['content'].split('.')[0])/100
+        currency_obj = soupobj.find('span', attrs={'itemprop': 'pricecurrency'})
+        currency = currency_obj['content']
+        return (price, currency)
+
+    @staticmethod
+    def _extract_wkn_isin(soupobj):
+        description_obj = soupobj.find('meta', attrs={'name': 'description'})
+        content_desc = description_obj['content']
+        wkn = content_desc.split('WKN')[1].split('|')[0].strip()
+        isin = content_desc.split('ISIN')[1].split('|')[0].strip()
+        return (wkn, isin)
+
     def _find_stock_url(self, searchparameter):
         ariva_urls = self.request_session.get('https://www.ariva.de/search/livesearch.m', params={'searchname':searchparameter})
         soup = BeautifulSoup(ariva_urls.text, 'html.parser')
@@ -25,4 +54,4 @@ class ArrivaScraper:
 
 if __name__ == '__main__':
     scraper = ArrivaScraper()
-    scraper._find_stock_url('APPLE')
+    scraper.scrape('APPLE')
